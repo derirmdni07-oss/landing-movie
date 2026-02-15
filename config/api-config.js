c0ec9597ad8228811c58f1d66c19f0158c547b3d
derirmdni07-oss <derirmdni.07@gmail.com>
2026-02-15 15:08:44 +0700

    Delete config directory

--- config/api-config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    API_KEY: '22be462e6d3de1dbab03d1ca50847b5a', // Dapatkan di themoviedb.org
-    BASE_URL: 'https://api.themoviedb.org/3',
-    IMG_URL: 'https://image.tmdb.org/t/p/original',
-    W500_URL: 'https://image.tmdb.org/t/p/w500',
-    CPA_URL: 'https://playfloowz.com/c?a=275&o=53' // Link iklan/CPA
-};
